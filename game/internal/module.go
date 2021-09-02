13a6f1144bafb78023a26578d51df858a03635af
tomas <tomas@gamstudio.net>
2021-09-02 17:45:15 +0800

    注释测试数据

--- game/internal/module.go
+++ game/internal/module.go
@@ -24,18 +24,22 @@ func (m *Module) OnInit() {
 	packageTax = make(map[uint16]float64)
 
 	// todo
-	//InitMongoDB()
-	//
-	//// 中心服初始化,主动请求Token
-	//c2c.Init()
-	//c2c.CreatConnect()
-	//
-	//go hall.HandleRoomData()
-	//
-	//go StartHttpServer()
+	InitMongoDB()
+
+	// 中心服初始化,主动请求Token
+	c2c.Init()
+	c2c.CreatConnect()
+
+	go hall.HandleRoomData()
+
+	go StartHttpServer()
 
 }
 
 func (m *Module) OnDestroy() {
-
+	hall.UserRecord.Range(func(key, value interface{}) bool {
+		p := value.(*Player)
+		c2c.UserLogoutCenter(p.Id, p.Password, p.Token)
+		return true
+	})
 }
