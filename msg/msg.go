e8db390bc5c9fc3ee074c46814babffd3c80530e
tomas <tomas@gamstudio.net>
2021-09-17 21:08:23 +0800

    判断金币是否存在不相同和添加log打印

--- msg/msg.go
+++ msg/msg.go
@@ -35,12 +35,3 @@ func init() {
 	Processor.Register(&SendCoordinate_C2S{})
 	Processor.Register(&ErrorMsg_S2C{})
 }
-
-type s struct {
-	data interface{}
-}
-
-func test() {
-	a := &s{}
-	b := a.data.([]int)
-}
